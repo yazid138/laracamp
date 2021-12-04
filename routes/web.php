7f9975c4e58fbaafe337b7f07f8fe9aec92ad7c4
yazid138 <ma39isy@gmail.com>
2021-12-04 18:16:49 +0700

    selesai slicing

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+
+Route::get('/checkout', function () {
+    return view('checkout');
+})->name('checkout');
+
+Route::get('/success-checkout', function () {
+    return view('success_checkout');
+})->name('success-checkout');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
