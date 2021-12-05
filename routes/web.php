a8ee80cb136950a14055daa37cb6c4f21a185855
yazid138 <ma39isy@gmail.com>
2021-12-05 07:57:15 +0700

    install breeze

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,9 @@ Route::get('/success-checkout', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__ . '/auth.php';
