12faf3150bbaaeffb370ef729fea3b41d59f477a
yazid138 <ma39isy@gmail.com>
2021-12-05 07:28:27 +0700

    push aja dah

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,5 @@
 <!doctype html>
 <html lang="en">
-
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
@@ -15,7 +14,9 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
 
-    <title>{{ $title ?? 'Laracamp' }}</title>
+    {{ $css ?? '' }}
+
+    <title>{{ $title ?? 'Laracamp by BuildWith Angga' }}</title>
 </head>
 
 <body>
@@ -30,6 +31,8 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous">
     </script>
+
+    {{ $js ?? '' }}
 
 </body>
 
